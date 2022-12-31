78037f9b0721fdaf8a2b9d059a9fbf3952e4beab
med <med.ben.amor31@gmail.com>
2022-12-31 23:21:32 +0100

    44. Splitting the Navigation Into Components

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  @Output()
+  sidenavToggle = new EventEmitter<void>();
+  ngOnInit(): void {
+  }
+  onToggleSideNav() {
+    this.sidenavToggle.emit();
+  }
 }
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent {
+export class SidenavListComponent implements OnInit {
+   @Output() sidenavClose = new EventEmitter<void>();
+  onCloseSidenav() {
+    this.sidenavClose.emit();
+  }
 
+  ngOnInit(): void {
+  }
 }
