a4f769d896681b312c3c1034af876f3c6b2a8373
med31 <med.ben.amor31@gmail.com>
2022-12-31 17:38:05 +0100

    adding navigationItem https://material.angular.io/components/list/overview#navigation-lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {MatNativeDateModule} from "@angular/material/core";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   exports: [
     BrowserModule,
