3f1f524e087ef95b47084cb5b0627a2049ea0b0b
med31 <med.ben.amor31@gmail.com>
2023-01-01 12:39:33 +0100

    add component currentTraining manage the visibities of different components

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-new-training',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent {
+  @Output()
+  startTraining = new EventEmitter<void>();
 
+  onStartTraining() {
+    this.startTraining.emit();
+  }
 }
